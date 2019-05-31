526065574ec653a160ec5d6fbee450ac327e9fc2
Tatianna-Gilliam <gilliamtatiannam@gmail.com>
2019-05-30 17:22:06 -0700

    The first push
    Work on article page

--- Article/Article.js
+++ Article/Article.js
@@ -3,20 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement= domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelectorAll(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-  }
+    this.expandButton.addEventListener('click', () =>{
+    this.expandArticle();
+    })
+  
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+   this.domElement.classList.toggle(article-span);
   }
 }
+
 
 /* START HERE: 
 
@@ -26,4 +29,5 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article");
+console.log(articles)
